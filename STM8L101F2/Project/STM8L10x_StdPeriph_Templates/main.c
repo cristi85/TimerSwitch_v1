762e16e8c9d5cec62a26892a05617ac2c872229f
cristi85 <ileacristian85@gmail.com>
2015-10-26 15:49:16 +0200

    Update main.c

--- STM8L101F2/Project/STM8L10x_StdPeriph_Templates/main.c
+++ STM8L101F2/Project/STM8L10x_StdPeriph_Templates/main.c
@@ -35,73 +35,52 @@
   */
 
 /* Private define ------------------------------------------------------------*/
-#define LIGHTSTATE_ON              (u8)1
-#define LIGHTSTATE_OFF             (u8)0
-#define READROM_U8(adr)            (u8)(*((u8 *)(u32)(adr)))
-#define READROM_U16(adr)           (u16)(*((u16 *)(u32)(adr)))
-#define READROM_U32(adr)           (u32)(*((u32 *)(u32)(adr)))
+#define LOAD_ON                    (u8)1
+#define LOAD_OFF                   (u8)0
 #define BLINK_REDLED(x)            {blink_redLED_times=(u8)x;   ((x==255)?(flag_blink_unlimited=TRUE):(flag_blink_unlimited=FALSE)); flag_blink_on_off=TRUE; cnt_state_redLED=0; LED_RED_ON; flag_blink_redLED=TRUE;}
-#define BLINK_GREENLED(x)          {blink_greenLED_times=(u8)x; ((x==255)?(flag_blink_unlimited=TRUE):(flag_blink_unlimited=FALSE)); flag_blink_on_off=TRUE; cnt_state_greenLED=0; LED_GREEN_ON; flag_blink_greenLED=TRUE;}
-#define BLINKSTOP_REDLED           {flag_blink_redLED=FALSE; LED_OFF;}
-#define BLINKSTOP_GREENLED         {flag_blink_greenLED=FALSE; LED_OFF;}
-#define ISBLINKING_REDLED          (flag_blink_redLED)
-#define ISBLINKING_GREENLED        (flag_blink_greenLED)
-#define STATUS_LOWBATT             (u8)0x01
-#define HBRIDGE_CHARGE_TIME        (u16)1000  /* minimum H-Bridge capacitor charge time [ms] */
-#define HBRIDGE_ON_TIME            (u8)100    /* H-Bridge conduction time [ms] */
-#define MAX_NUM_RECEIVERS          (u8)10
-#define BTN1_DELETE_ID_THR         (u16)3000
+#define BLINK_GREENLED(x)           {blink_greenLED_times=(u8)x; ((x==255)?(flag_blink_unlimited=TRUE):(flag_blink_unlimited=FALSE)); flag_blink_on_off=TRUE; cnt_state_greenLED=0; LED_GREEN_ON; flag_blink_greenLED=TRUE;}
+#define BLINKSTOP_REDLED            {flag_blink_redLED=FALSE; LED_OFF;}
+#define BLINKSTOP_GREENLED          {flag_blink_greenLED=FALSE; LED_OFF;}
+#define ISBLINKING_REDLED           (flag_blink_redLED)
+#define ISBLINKING_GREENLED         (flag_blink_greenLED)
+#define HBRIDGE_CHARGE_TIME         (u16)1000  /* minimum H-Bridge capacitor charge time [ms] */
+#define HBRIDGE_ON_TIME             (u8)100    /* H-Bridge conduction time [ms] */
+#define BTN1_SET_NEW_TIME           (u16)3000  /* 3000ms */
+#define TIMER_VAL_DEFAULT           (u16)300   /* 300 seconds */
+#define TIMER_VAL_PROGRAMMING_START (u16)1     /* 1 minute */
+
 /* Private typedef -----------------------------------------------------------*/
-typedef union
-{
-  struct
-  {
-    u8 b0;
-    u8 b1;
-    u8 b2;
-    u8 b3;
-  } B;
-  u32 DW;
-} tBtoDW;
 typedef enum States 
 {
-  ST_INIT                      = 0,
-  ST_WAIT_INPUT_OR_FLAG        = 1,
-  ST_NRF_RCVD_DATA             = 2,
-  ST_SWITCH_LIGHT              = 3,
-  ST_WAIT_TIMEOUT1_CAP_CHARGE  = 4,
-  ST_WAIT_TIMEOUT1_HBRIDGE_ON  = 5
+  ST_INIT               = 0,
+  ST_WAIT_INPUT         = 1,
+  ST_SWITCH_LOAD_ON     = 2,
+  ST_SWITCH_LOAD_OFF    = 3,
+  ST_WAIT_CAP_CHARGE    = 4,
+  ST_WAIT_HBRIDGE_ON    = 5,
+  ST_PROGRAMMING_MODE   = 6
 } StatesType;
 
 /* Private macro -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
-static u8 LightState = LIGHTSTATE_OFF;
-static tBtoDW BtoDW;
-static _Bool LrnModeActive = FALSE;
+static u8 LoadStateRequest = LOAD_OFF;
+static u8 LoadState = LOAD_OFF;
 static _Bool FLAG_BTN1_lock = FALSE;
 static _Bool FLAG_reset_LEDblink_error = FALSE;
-static _Bool FLAG_check_nrf_reg = FALSE;
-static u8 error_nrf24_reg_cnt = 0;
+static _Bool FLAG_programming_mode = FALSE;
+static u8 programming_mode_step = 0;
 static volatile StatesType state = ST_INIT;
-static volatile u8 program_status = 0;
-static volatile u8 interrupt_status = 0;
-static volatile u8 debug_registers[8];
-static u8 debug_reg_idx = 0;
 static u8 task_1000ms_cnt = 0;
-static u16 init_delay = U16_MAX;
-static volatile u8 flash_erase_timing_test;
+static u16 timer_cnt_seconds = 0;
+static _Bool FLAG_timer_on = FALSE;
+static u16 timer_value = TIMER_VAL_PROGRAMMING_START;
+static const u16 timer_val_stored = TIMER_VAL_DEFAULT;
 /* Private function prototypes -----------------------------------------------*/
 /* Private functions ---------------------------------------------------------*/
-void TASK_1000ms(void);
-void TASK_500ms(void);
-void TASK_250ms(void);
 void BTN1_Released(void);
-void LearnNewID(void);
-_Bool IsLearnedID(void);
 
 // RUNTIME MEASUREMENT
 RTMS_DECLARE(runtime_it_1ms);
-RTMS_DECLARE(runtime_it_RFrcv);
 /**
   * @brief  Main program.
   * @param  None
@@ -109,32 +88,25 @@ RTMS_DECLARE(runtime_it_RFrcv);
   */
 void main(void)
 {
-  flash_erase_timing_test = 0;
-  program_status = 1;
   disableInterrupts();
   Config();
-  program_status = 2;
   Errors_Init();
   RTMS_INIT(runtime_it_1ms);
-  RTMS_INIT(runtime_it_RFrcv);
   enableInterrupts();
   LED_GREEN_ON;
+  LED_RED_ON;
   /* Wait for power supply settling */
   Timeout_SetTimeout2(200);
   while(!Timeout_IsTimeout2());
   LED_OFF;
-  program_status = 3;
-  if(RST_GetFlagStatus(RST_FLAG_IWDGF))
-  {
+  if(RST_GetFlagStatus(RST_FLAG_IWDGF)) {
     BLINK_REDLED(1);
   }
-  else if(RST_GetFlagStatus(RST_FLAG_ILLOPF))
-  {
+  else if(RST_GetFlagStatus(RST_FLAG_ILLOPF)) {
     BLINK_REDLED(2);
   }
   RST_ClearFlag(RST_FLAG_POR_PDR | RST_FLAG_SWIMF | RST_FLAG_ILLOPF | RST_FLAG_IWDGF);
   while(ISBLINKING_REDLED);
-  program_status = 4;
   HBRIDGE_OFF;
   Timeout_SetTimeout1(HBRIDGE_CHARGE_TIME);
   
@@ -144,23 +116,17 @@ void main(void)
   IWDG_SetReload(0xFF);
   IWDG_WriteAccessCmd(IWDG_WriteAccess_Disable);
   IWDG_ReloadCounter();
-  program_status = 5;
   while (1)
   {
     switch(state)
     {
       case ST_INIT:
       {
-        state = ST_WAIT_INPUT_OR_FLAG;
+        state = ST_WAIT_INPUT;
         break;
       }
-      case ST_WAIT_INPUT_OR_FLAG:
-      {  
-        //state = ST_WAIT_TIMEOUT1_CAP_CHARGE;
-        if(FLAG_1000ms)
-        {
-          FLAG_1000ms = FALSE;
-        }
+      case ST_WAIT_INPUT:
+      {
         /* ============== PRESS BTN1 with key repetition lock ================= */
         if(BTN1_DEB_STATE == BTN_PRESSED && !FLAG_BTN1_lock)
         {
@@ -172,29 +138,50 @@ void main(void)
           BTN1_Released();
         }
         /* ============== END PRESS BTN1 with key repetition lock ================= */
-        
-        break;
-      }
-      case ST_SWITCH_LIGHT:
-      {
-        LED_GREEN_ON;
-        LIGHT_ON;
-        LightState = LIGHTSTATE_ON;
-
-        /* set timeout for H-Bridge ON */
-        Timeout_SetTimeout1(HBRIDGE_ON_TIME);
-        state = ST_WAIT_TIMEOUT1_HBRIDGE_ON;
-        break;
-      }
-      case ST_WAIT_TIMEOUT1_CAP_CHARGE:
-      {
-        if(Timeout_IsTimeout1())
+        if(timer_cnt_seconds >= *((u16*)(&timer_val_stored)))
         {
-          state = ST_SWITCH_LIGHT;
+          FLAG_timer_on = FALSE;
+          timer_cnt_seconds = 0;
+          LoadStateRequest = LOAD_OFF;
+          state = ST_WAIT_CAP_CHARGE;
         }
         break;
       }
-      case ST_WAIT_TIMEOUT1_HBRIDGE_ON:
+      case ST_SWITCH_LOAD:
+      {
+        switch(LoadStateRequest)
+        {
+          case LOAD_OFF:
+          {
+            LED_GREEN_OFF;
+            LOAD_OFF;
+            LoadState = LOAD_OFF;
+            break;
+          }
+          case LOAD_ON:
+          {
+            LED_GREEN_ON;
+            LOAD_ON;
+            LoadState = LOAD_ON;
+            FLAG_timer_on = TRUE;
+            timer_cnt_seconds = 0;
+            break;
+          }
+          default: break;
+        }
+        Timeout_SetTimeout1(HBRIDGE_ON_TIME);  /* set timeout for H-Bridge ON */
+        state = ST_WAIT_HBRIDGE_ON;
+        break;
+      }
+      case ST_WAIT_CAP_CHARGE:
+      {
+        if(Timeout_IsTimeout1())
+        {
+          state = ST_SWITCH_LOAD;
+        }
+        break;
+      }
+      case ST_WAIT_HBRIDGE_ON:
       {
         if(Timeout_IsTimeout1())
         {
@@ -202,11 +189,77 @@ void main(void)
           /* set timeout for H-Bridge capacitor to charge */
           Timeout_SetTimeout1(HBRIDGE_CHARGE_TIME);
           BLINKSTOP_GREENLED;
-          state = ST_WAIT_INPUT_OR_FLAG;
+          state = ST_WAIT_INPUT;
         }
         break;
       }
       default: break;
+    }
+    if(FLAG_programming_mode)
+    {
+      switch(programming_mode_step)
+      {
+        case 0:
+        {
+          if(timer_value < 10) {
+            BLINK_REDLED(timer_value);
+          }
+          else if(timer_value < 100) {
+            BLINK_REDLED(timer_value / 10);            
+          }
+          programming_mode_step = 1;
+          break;
+        }
+        case 1:
+        {
+          if(!ISBLINKING_REDLED) {
+            if(timer_value < 10) {
+              Timeout_SetTimeout2(1000);
+              programming_mode_step = 5;
+            }
+            else if(timer_value < 100) {
+              Timeout_SetTimeout2(300);
+              programming_mode_step = 2;
+            }
+          }
+          break;
+        }
+        case 2:
+        {
+          if(Timeout_IsTimeout2()) {
+            programming_mode_step = 3;
+          }
+          break;
+        }
+        case 3:
+        {
+          BLINK_REDLED(timer_value % 10);
+          programming_mode_step = 4;
+          break;
+        }
+        case 4:
+        {
+          if(!ISBLINKING_REDLED) {
+            Timeout_SetTimeout2(1000);
+            programming_mode_step = 5;
+          }
+          break;
+        }
+        case 5:
+        {
+          if(Timeout_IsTimeout2()) {
+            programming_mode_step = 0;
+          }
+          break;
+        }
+        default: break;
+      }
+    }
+    if(FLAG_1000ms)
+    {
+      FLAG_1000ms = FALSE;
+      task_1000ms_cnt++;
+      if(FLAG_timer_on) timer_cnt_seconds++;
     }
     IWDG_ReloadCounter();
     if(Errors_IsError() && !FLAG_reset_LEDblink_error)
@@ -227,18 +280,42 @@ void main(void)
 
 void BTN1_Released()
 {
-  if(BTN1_press_timer < BTN1_DELETE_ID_THR)
+  if(BTN1_press_timer < BTN1_SET_NEW_TIME)
   {
-  
+    if(FLAG_programming_mode) {
+      if(timer_value < 99) timer_value++;
+    }
+    else {
+      LoadStateRequest = LOAD_ON;
+      state = ST_WAIT_CAP_CHARGE;
+    }
+  }
+  else
+  {
+    if(FLAG_programming_mode) {
+      FLAG_programming_mode = FALSE;
+      Program_Timer_Value();
+    }
+    else {
+      FLAG_programming_mode = TRUE;
+      timer_value = TIMER_VAL_PROGRAMMING_START;
+    }
   }
   BTN1_press_timer = 0;
 }
 
-void TASK_1000ms()
+void Program_Timer_Value()
 {
-  task_1000ms_cnt++;
+  FLASH_Unlock(FLASH_MemType_Program);
+  FLASH_ProgramByte((u16)(u8*)(&timer_val_stored), (u8)(timer_value >> 8);
+  FLASH_ProgramByte((u16)(u8*)(&Receivers.NumLrndRcv), (u8)(timer_value & (u16)0x00FF));
+  FLASH_Lock(FLASH_MemType_Program);
+  /* Check what was written */
+  if( timer_value != *((u16*)(&timer_val_stored)) )
+  {
+    Errors_SetError(ERROR_FLASH_WRITE);
+  }
 }
-
 #ifdef  USE_FULL_ASSERT
 
 /**
